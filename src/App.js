13af9be1c265109073946fa310a27d18317b68a7
sohyeon403 <sohyun9743@naver.com>
2021-03-27 16:29:11 +0900

    2021-03-27
    [git start]

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+class App extends React.Component{
+  state = {
+    count : 0
+  };
+
+  add= () => {
+    console.log("add");
+  };
+  minus = () => {
+    console.log("minus");
+  };
+  
+
+  render(){
+    return (
+      <div>
+        <h1>The number is :  {this.state.count}</h1>
+        <button onClick={this.add}>add</button>
+        <button onClick={this.minus}>minus</button>
+      </div>
+    );
+  }
+}
+
+export default App;
