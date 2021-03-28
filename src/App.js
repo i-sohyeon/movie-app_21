c84599ebea351ffb1d5b14ee5062e5731fa7cfa1
sohyeon403 <sohyun9743@naver.com>
2021-03-28 17:03:28 +0900

    [update]
    axios download

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component{
 
   
   state = {
-    isLoading : true
+    isLoading : true,
+    movie: []
   };
 
+  getMovies = async () => {
+    const movies = await axios.get("http://yts-proxy.now.sh/list_movies.json");
+    console.log(movies);
+  }
+
   componentDidMount(){
-    setTimeout(() => {
-      this.setState({isLoading:false});
-    }, 6000)
+    this.getMovies();
   }
 
 
